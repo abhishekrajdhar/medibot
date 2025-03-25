9f65e2fb4ffc42a42f9a520fd284af3aa1d914cf
abhishekrajdhar <rajdhardubey4434@gmail.com>
2025-03-25 16:55:14 +0530

    final deployment

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI(title="MediBot AI Chat API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["my-medic.vercel.app"],  
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
